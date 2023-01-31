88383b02c95ab4d45f4cf7e31dc24382e58d950a
pdrocosta <pedro.augusto.costa.99@gmail.comgit>
2023-01-31 20:12:41 -0300

    index

---
+++ PCHamburgueria/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import {GlobalStyle} from './styles/global.js'
+import {ProductList} from ''
+import {Product} from ''
+import {Logo} from ''
+import {InputSearch} from ''
+import {Header} from ''
+import {CartTotal} from ''
+import {CartProduct} from ''
+import {Cart} from ''
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <GlobalStyle/>
+   
+    </div>
+  )
+}
+
+export default App
